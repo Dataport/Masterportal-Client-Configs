1d27176af99652b07c92dbe23a6ae80e62fcb7e2
bruehlca <carina.bruehl@dataport.de>
2025-01-15 14:03:26 +0100

    replace spaces with tabs

--- app/tracking.js
+++ app/tracking.js
@@ -5,8 +5,8 @@ import { URL, fileURLToPath } from 'node:url'
 const hitRatePerPortal = {}
 
 export function countVisit(reqParamsVariant) {
-    if(hitRatePerPortal.hasOwnProperty(reqParamsVariant)) {
-		hitRatePerPortal[reqParamsVariant] += 1;
+	if(hitRatePerPortal.hasOwnProperty(reqParamsVariant)) {
+	hitRatePerPortal[reqParamsVariant] += 1;
 	}
 }
 
@@ -25,43 +25,43 @@ function setHitRatePerPortal(labels, via, prefix = '') {
 
 function sendRequestCountsToApi() {
 	const data = JSON.stringify({
-        series: Object.keys(hitRatePerPortal).map(portal => ({
-            metric: 'portal.hit.rates',
-            points: [
+		series: Object.keys(hitRatePerPortal).map(portal => ({
+		metric: 'portal.hit.rates',
+			points: [
 			{
 				timestamp: Math.floor(Date.now() / 1000), 
 				value: hitRatePerPortal[portal]
 			}
 			],
-            tags: [`portal:${portal}`]
-        }))
-    });
+			tags: [`portal:${portal}`]
+		}))
+	});
 
-    fetch(process.env.TRACKING_API_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(data, 'utf8').toString(),
-            [process.env.TRACKING_API_KEY_NAME]: process.env.TRACKING_API_KEY
-        },
-        body: data
-    })
-    .then(response => response.text())
-    .then(responseData => {
-        console.log('Hit rates sent to Monitoring API', responseData);
-        Object.keys(hitRatePerPortal).forEach(portal => {
-            hitRatePerPortal[portal] = 0;
-        });
-    })
-    .catch(error => {
-        console.error('Error sending request counts to API:', error);
-    });
+	fetch(process.env.TRACKING_API_URL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': Buffer.byteLength(data, 'utf8').toString(),
+			[process.env.TRACKING_API_KEY_NAME]: process.env.TRACKING_API_KEY
+		},
+		body: data
+	})
+	.then(response => response.text())
+	.then(responseData => {
+		console.log('Hit rates sent to Monitoring API', responseData);
+		Object.keys(hitRatePerPortal).forEach(portal => {
+			hitRatePerPortal[portal] = 0;
+		});
+	})
+	.catch(error => {
+		console.error('Error sending request counts to API:', error);
+	});
 }
 
 export function initTracking() {
-    const clientConfig = JSON.parse(readFileSync(fileURLToPath(new URL('../resources/clients.json', import.meta.url)), 'utf-8'))
-    setHitRatePerPortal(clientConfig.variantLabels, clientConfig.via)
-    
-    const interval = process.env.TRANSFER_INTERVAL || 3600000;
-    setInterval(sendRequestCountsToApi, interval);
+	const clientConfig = JSON.parse(readFileSync(fileURLToPath(new URL('../resources/clients.json', import.meta.url)), 'utf-8'))
+	setHitRatePerPortal(clientConfig.variantLabels, clientConfig.via)
+
+	const interval = process.env.TRANSFER_INTERVAL || 3600000;
+	setInterval(sendRequestCountsToApi, interval);
 }
